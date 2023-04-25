4da32b2134d6e2f5741f6bcc33ac55914b5e699c
Gamze Turap <gamzeturap1@gmail.com>
2023-04-25 11:31:40 +0300

    Login

--- AddressBookPL/Controllers/AccountController.cs
+++ AddressBookPL/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using AddressBookEL.IdentityModels;
 using AddressBookPL.Models;
+using Microsoft.AspNetCore.Http.Metadata;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,6 +11,14 @@ namespace AddressBookPL.Controllers
         private readonly UserManager<AppUser>_userManager;
         private readonly RoleManager<AppRole> _roleManager;
         private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+        }
+
         [HttpGet]
         public IActionResult Register()
         {
@@ -20,14 +29,60 @@ namespace AddressBookPL.Controllers
         {
             try
             {
-                return RedirectToAction("Login", "Account");
+                if (!ModelState.IsValid)
+                {
+                    return View();
+                }
+                //ayni userName'den varsa hata versin
+                var sameUser = _userManager.FindByNameAsync(model.UserName).Result; //async bir metodun sonuna .Result yazarsak method senkron calisir.
+                if (sameUser!=null)
+                {
+                    ModelState.AddModelError("", "Bu kullanici sistemde mevcuttur! Farkli kullanici adi deneyiniz.");
+                }
+                //ayni userName'den varsa hata versin.
+                sameUser = _userManager.FindByEmailAsync(model.UserName).Result; //async bir metodun sonuna .Result yazarsak method senkron calisir.
+                if (sameUser != null)
+                {
+                    ModelState.AddModelError("", "Bu kullanici sistemde mevcuttur! Farkli kullanici adi deneyiniz.");
+                }
+                //Artik sisteme kayit olabilir.
+                AppUser user = new AppUser()
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    PhoneNumber = model.Phone,
+                    Email = model.Email,
+                    UserName = model.UserName,
+                    CreatedDate = DateTime.Now,
+                    EmailConfirmed = true, //dogrula yapmis kabul ettik.
+                    IsPassive = false
+                };
+                if (model.Birthdate!=null)
+                {
+                    user.Birthdate = model.Birthdate;
+                }//user kaydedilsin
+                var Result = _userManager.CreateAsync(user, model.Password).Result;
+                if (Result.Succeeded)
+                {
+                    //Kullanici olusursa ona rol atamasi yapilacak!
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Ekleme basarisiz!");
+                    return View(model);
+                }
             }
             catch (Exception ex)
             {
-
                 ModelState.AddModelError("", "Beklenmedik hata olustu!");
                 return View(model);
             }
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
         }
     }
 }
---
+++ AddressBookPL/Models/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AddressBookPL.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string UserNameorEmail { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
