2c181cae8b57b9d73be1a76f24cdd0fbce699921
Gamze Turap <gamzeturap1@gmail.com>
2023-04-25 10:00:33 +0300

    Register

--- AddressBookPL/Controllers/AccountController.cs
+++ AddressBookPL/Controllers/AccountController.cs
@@ -1,13 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AddressBookEL.IdentityModels;
+using AddressBookPL.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AddressBookPL.Controllers
 {
-    public class AccountController1 : Controller
+    public class AccountController : Controller
     {
+        private readonly UserManager<AppUser>_userManager;
+        private readonly RoleManager<AppRole> _roleManager;
+        private readonly SignInManager<AppUser> _signInManager;
         [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            try
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            catch (Exception ex)
+            {
+
+                ModelState.AddModelError("", "Beklenmedik hata olustu!");
+                return View(model);
+            }
         }
     }
 }
---
+++ AddressBookPL/Models/RegisterViewModel.cs
@@ -0,0 +1,32 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace AddressBookPL.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string UserName { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(11, MinimumLength = 11)]
+        //Regular Expression yazilmali
+        public string Phone { get; set; }
+        public DateTime? Birthdate { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password")]
+        public string PasswordConfirm { get; set; }
+
+
+
+    }
+}
