6d72b4942b97672fb209add1a5d43a2966093230
Gamze Turap <gamzeturap1@gmail.com>
2023-04-27 08:56:19 +0300

    Xihan

--- AddressBookPL/DefaultData/DataDefaultXihan.cs
+++ AddressBookPL/DefaultData/DataDefaultXihan.cs
@@ -1,4 +1,6 @@
-﻿using AddressBookEL.IdentityModels;
+﻿using AddressBookBL.InterfacesOfManagers;
+using AddressBookEL.IdentityModels;
+using ClosedXML.Excel;
 using Microsoft.AspNetCore.Identity;
 
 namespace AddressBookPL.DefaultData
@@ -32,6 +34,29 @@ namespace AddressBookPL.DefaultData
             {
                 // ex loglanabilİr
                 // yazılımcıya acil başlıklı email gönderilebilir
+            }
+        }
+        public void CreateAllCities(ICityManager cityManager)
+        {
+            try
+            {
+                //1) veritabanindaki illeri listeye alalim
+                //2)Exceli acip satir satir okuyup
+                //2.5) olmayan ili veritabanina ekleyelim.
+
+                var cityList = cityManager.GetAll(x => !x.IsRemoved).Data;//1
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Excels");
+                string fileName = Path.GetFileName("Cities.xlsx"); //?
+                string filePath = Path.Combine(path, fileName);
+
+                using (var excelBook=new XLWorkbook(filePath))//C:Users/../wwwroot/Excels/Cities.xlsx
+                {
+                    var rows = excelBook.Worksheet(1).RowsUsed();//82 satir var
+                }
+            }
+            catch (Exception ex)
+            {
+                //loglanacak
             }
         }
 
